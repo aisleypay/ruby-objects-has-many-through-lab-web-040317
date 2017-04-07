a066faf7e39f624a111151ed276ee5c008c0847d
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2017-04-07 16:06:50 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+class Genre
+  attr_reader :name
+  attr_accessor :songs
+
+  def initialize (name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    self.songs.collect { |song|
+      song.artist
+    }
+  end
+end
